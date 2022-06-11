a369da9b4a1a5d256e9e1b509f15e905981ab0e4
Juan <juancheloar@gmail.com>
2022-06-11 20:13:18 -0300

    first commit

---
+++ Funciones/ing2i.py
@@ -0,0 +1,5 @@
+#Funcion que permite el ingreso de 2 valores enteros:
+def ing2i():
+    i1 = int(input("Ingrese el primer valor entero: "))
+    i2= int(input("Ingrese el segundo valor entero: "))
+    return[i2, i2]
---
+++ Funciones/presentacion.py
@@ -0,0 +1,2 @@
+def presentacion():
+    print("Primer Trabajo Colaborativo Aula 1 Grupo 2  - A4G3")
